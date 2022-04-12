41ce8005caa4d7f7114aa23f5bb89f18ee63d596
Thiago Gutenberg C. da Costa <thiaguten@gmail.com>
2022-04-11 21:55:47 -0300

    Containerizando o projeto Angular com o Docker

--- portal-web/src/environments/environment.prod.ts
+++ portal-web/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  idp_base_url: 'http://localhost:8080/auth',
+  api_servico_base_url: 'http://localhost:8200',
+  api_usuarios_base_url: 'http://localhost:8200',
+  api_ocorrencia_base_url: 'http://localhost:8200',
+  api_localizacao_base_url: 'http://localhost:8000'
 };
